283f924f79a5a648a3b4d1ef97f66f4b9e8001dd
pione30 <g.pionelish30@gmail.com>
2015-03-27 20:48:33 +0900

    add rep1

--- template.cpp
+++ template.cpp
@@ -8,6 +8,7 @@
 #include <set>
 #include <cmath>
 #define rep(i, n) for(int (i) = 0; (i) < (int)(n); ++(i))
+#define rep1(i, n) for(int (i) = 1; (i) <= (int)(n); ++(i))
 using namespace std;
 typedef long long int lli;
 typedef vector<int> vi;
