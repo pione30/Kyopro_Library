dce8dadbe83d0959e2b6f609aec7302a70d726f9
pione30 <g.pionelish30@gmail.com>
2015-09-03 19:01:26 +0900

    add mod_inverse

---
+++ mod_inverse.cpp
@@ -0,0 +1,19 @@
+typedef long long int lli;
+
+lli extgcd(lli a, lli b, lli& x lli& y){
+	lli d = a;
+	if (b != 0){
+		d = extgcd(b, a % b, y, x);
+		y -= (a / b) * x;
+	}
+	else{
+		x = 1; y = 0;
+	}
+	return d;
+}
+
+lli mod_inverse(lli a, lli m){
+  lli x, y;
+  extgcd(a, m, x, y);
+  return (m + x % m) % m;
+}
