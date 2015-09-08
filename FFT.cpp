8bdd08ac1be76f6d5bf83e7e06497587d470b077
pione30 <g.pionelish30@gmail.com>
2015-09-08 19:37:53 +0900

    add FFT

---
+++ FFT.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <complex>
+using namespace std;
+typedef complex<double> cmpd;
+
+const int FFT_N = 262144; // 2^18
+enum fft_direction { fft_forward, fft_backward };
+
+void dft(vector<cmpd>& f, int n, fft_direction fftd){
+  if(n == 1) return;
+
+  vector<cmpd> f0(n / 2);
+  vector<cmpd> f1(n / 2);
+  for(int i = 0; i < n / 2; i++){
+    f0[i] = f[2 * i + 0];
+    f1[i] = f[2 * i + 1];
+  }
+  dft(f0, n / 2, fftd);
+  dft(f1, n / 2, fftd);
+  cmpd zeta = polar(1.0, (fftd == fft_forward ? 1 : -1) * 2 * M_PI / n);
+  cmpd pow_zeta = 1;
+  for(int i = 0; i < n; i++){
+    f[i] = f0[i % (n / 2)] + pow_zeta * f1[i % (n / 2)];
+    pow_zeta *= zeta;
+  }
+}
+
