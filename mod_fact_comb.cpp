ff5070f40eac09d0165eaebb9d0f78320ababd6c
pione30 <g.pionelish30@gmail.com>
2015-09-13 23:48:51 +0900

    change int to lli

--- mod_fact_comb.cpp
+++ mod_fact_comb.cpp
@@ -1,17 +1,19 @@
-int MAX_P = 1000;
+typedef long long int lli;
+
+lli MAX_P = 1000;
 
 vi fact(MAX_P); // n! mod p のテーブル O(p)
 
-void init(int p){
+void init(lli p){
   fact[0] = 1;
   fact[1] = 1;
-  for(int i = 2; i < p; i++){
+  for(lli i = 2; i < p; i++){
     fact[i] = fact[i - 1] * i % p;
   }
 }
 
-int extgcd(int a, int b, int& x, int& y){
-	int d = a;
+lli extgcd(lli a, lli b, lli& x, lli& y){
+	lli d = a;
 	if (b != 0){
 		d = extgcd(b, a % b, y, x);
 		y -= (a / b) * x;
@@ -22,19 +24,19 @@ int extgcd(int a, int b, int& x, int& y){
 	return d;
 }
 
-int mod_inverse(int a, int m){
-  int x, y;
+lli mod_inverse(lli a, lli m){
+  lli x, y;
   extgcd(a, m, x, y);
   return (m + x % m) % m;
 }
 
 // n! = a * p^e としたときの a mod p を求める。O(log_p n)
-int mod_fact(int n, int p, int& e){
+lli mod_fact(lli n, lli p, lli& e){
   e = 0;
   if (n == 0) return 1;
 
   // pの倍数の部分を計算
-  int res = mod_fact(n / p, p, e);
+  lli res = mod_fact(n / p, p, e);
   e += n / p;
 
   // (p - 1)! ≡ -1 なので (p - 1)! ^ (n / p) は n / p の偶奇だけで計算できる
@@ -43,10 +45,10 @@ int mod_fact(int n, int p, int& e){
 }
 
 // nCk mod p を求める。 O(log_p n)
-int mod_comb(int n, int k, int p){
+lli mod_comb(lli n, lli k, lli p){
   if (n < 0 || k < 0 || n < k) return 0;
-  int e1, e2, e3;
-  int a1 = mod_fact(n, p, e1), a2 = mod_fact(k, p, e2), a3 = mod_fact(n - k, p, e3);
+  lli e1, e2, e3;
+  lli a1 = mod_fact(n, p, e1), a2 = mod_fact(k, p, e2), a3 = mod_fact(n - k, p, e3);
   if(e1 > e2 + e3) return 0;
   return a1 * mod_inverse(a2 * a3 % p, p) % p;
 }
