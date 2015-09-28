d7ebf79de234d8922594c1b7747c53aa3cc0e666
pione30 <g.pionelish30@gmail.com>
2015-09-28 12:44:19 +0900

    add all macro

--- template.cpp
+++ template.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for(int (i) = 0; (i) < (int)(n); ++(i))
 #define rep1(i, n) for(int (i) = 1; (i) <= (int)(n); ++(i))
+#define all(a) (a).begin(),(a).end()
 using namespace std;
 typedef long long int lli;
 typedef vector<int> vi;
