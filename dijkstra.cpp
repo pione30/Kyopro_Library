85ae62730dfaebff0229bc1f5b6633234b202eeb
pione30 <g.pionelish30@gmail.com>
2015-12-09 13:22:14 +0900

    edit initialization

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,15 +3,14 @@ const int INF = 0x7FFFFFFF - 10;
 struct edge {int to, cost; };
 typedef pair<int, int> P;
 
-int V;
 const int MAX_V = 100;
 vector<edge> G[MAX_V]; // グラフを表す
-vi d(MAX_V); // 始点からの最小コストが格納される
+vi d(MAX_V, INF); // 始点からの最小コストが格納される
 
 void dijkstra(int s){
   // greater<P>を指定することでfirstが小さい順に取り出せるようにする
   priority_queue<P, vector<P>, greater<P> > que;
-  fill(d.begin(), d.begin() + V, INF);
+  // fill(all(d), INF);
   d[s] = 0;
   que.push(P(0, s));
 
