772e3c350407a1d96dac0da41a5a6cb1fc6dbb65
pione30 <g.pionelish30@gmail.com>
2015-12-22 11:13:44 +0900

    tab to 2spaces

--- BinaryIndexedTree.cpp
+++ BinaryIndexedTree.cpp
@@ -6,17 +6,17 @@ int n;
 // [1, n]
 
 int bit_sum(int i){
-	int s = 0;
-	while (i > 0){
-		s += bit[i];
-		i -= i & -i;
-	}
-	return s;
+  int s = 0;
+  while (i > 0){
+    s += bit[i];
+    i -= i & -i;
+  }
+  return s;
 }
 
 void bit_add(int i, int x){
-	while (i <= n){
-		bit[i] += x;
-		i += i & -i;
-	}
+  while (i <= n){
+    bit[i] += x;
+    i += i & -i;
+  }
 }
--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -4,24 +4,24 @@ const int MAX_N = 262144; //2^18
 vector<int> segtree(2 * MAX_N - 1);
 
 void st_add(int x){
-	int n = MAX_N - 1 + x;
-	while (n > 0){
-		++segtree[n];
-		n = (n - 1) / 2;
-	}
+  int n = MAX_N - 1 + x;
+  while (n > 0){
+    ++segtree[n];
+    n = (n - 1) / 2;
+  }
 }
 
 int st_eject(int x){
-	int n = 0;
-	while (2 * n + 1 < 2 * MAX_N - 1){
-		if (segtree[2 * n + 1] < x){
-			x -= segtree[2 * n + 1];
-			n = 2 * n + 2;
-		}
-		else{
-			n = 2 * n + 1;
-		}
-		--segtree[n];
-	}
-	return n - (MAX_N - 1);
+  int n = 0;
+  while (2 * n + 1 < 2 * MAX_N - 1){
+    if (segtree[2 * n + 1] < x){
+      x -= segtree[2 * n + 1];
+      n = 2 * n + 2;
+    }
+    else{
+      n = 2 * n + 1;
+    }
+    --segtree[n];
+  }
+  return n - (MAX_N - 1);
 }
